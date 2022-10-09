2ece78def5df020cacac3ba398ed49762142b263
Alvin Francis <nivlafx@gmail.com>
2022-10-09 17:01:48 -0400

    rops: set raven as default GMS override prop
    test: m, safetynet passes
    Change-Id: If8f98e1531421d886761123cdb09ade268769db1

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -15,7 +15,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Spoof fingerprint for Google Play Services and SafetyNet
 ifeq ($(PRODUCT_OVERRIDE_GMS_FINGERPRINT),)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=google/raven/raven:13/TP1A.221005.002/9012097:user/release-keys
 else
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.build.gms_fingerprint=$(PRODUCT_OVERRIDE_GMS_FINGERPRINT)
 endif
