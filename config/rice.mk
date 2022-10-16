23b94e77c1e385075dae9199eca52f70fc7b7e02
Alvin Francis <nivlafx@gmail.com>
2022-10-16 10:06:43 -0400

    Fix bootanimation after initial boot
    Change-Id: I89adec6bfc47d9163ed6c322eb16b9e521878b7f

--- config/rice.mk
+++ config/rice.mk
@@ -94,15 +94,15 @@ endif
 # Bootanimation
 ifeq ($(SUSHI_BOOTANIMATION),720)
 # Sushi Bootanimation 720 by saavedra3
-PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(SUSHI_BOOTANIMATION),1080)
 # Sushi Bootanimation 1080 by saavedra3
-PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(SUSHI_BOOTANIMATION),1440)
 # Sushi Bootanimation 1440 by saavedra3
-PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/sushi_bootanimation_1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else
-PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
