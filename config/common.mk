e686d55b540d4f55f80ca273d48c8932591cb549
John Galt <johngaltfirstrun@gmail.com>
2022-11-16 05:48:38 +0800

    config/common: dedupe vndk libraries
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -209,6 +209,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
     SystemUI
 
+
+# Dedupe VNDK libraries with identical core variants
+TARGET_VNDK_USE_CORE_VARIANT := true
+
 # Use a generic profile based boot image by default
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-profile.txt
